22bd99234f14255daa85b175f20eca86a5aaa94e
Eric Gibby <eric.gibby@imaginelearning.com>
2021-05-30 11:46:25 -0600

    Wired up Modal's onRequestClose with EditContact's onCancel

--- src/components/EditContactModal/EditContactModal.tsx
+++ src/components/EditContactModal/EditContactModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal, { ModalProps } from '../Modal/Modal';
 import EditContact, { EditContactProps } from '../EditContact/EditContact';
 
-export type EditContactModalProps = ModalProps & EditContactProps;
+export type EditContactModalProps = Omit<ModalProps, 'onRequestClose'> & EditContactProps;
 
 export default function EditContactModal({
 	ariaHideApp,
@@ -10,7 +10,6 @@ export default function EditContactModal({
 	initialValues,
 	isOpen,
 	onCancel,
-	onRequestClose,
 	onSubmit,
 }: EditContactModalProps) {
 	return (
@@ -18,7 +17,7 @@ export default function EditContactModal({
 			className={className}
 			isOpen={isOpen}
 			ariaHideApp={ariaHideApp}
-			onRequestClose={onRequestClose}
+			onRequestClose={onCancel}
 		>
 			<h1 className="mb-4 font-bold text-xl">{`${initialValues ? 'Edit' : 'Add'} Contact`}</h1>
 			<EditContact initialValues={initialValues} onCancel={onCancel} onSubmit={onSubmit} />
