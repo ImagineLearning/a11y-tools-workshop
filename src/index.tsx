a2858f5115ae74949723685af2762fb596c5da6b
Eric Gibby <eric.gibby@imaginelearning.com>
2021-05-30 11:47:32 -0600

    Set app element for ReactModal

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ReactModal from 'react-modal';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -15,3 +16,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+ReactModal.setAppElement('#root');
